9db2b49b4dd1a3ec5e991908ac4c9ec1b148a1ba
Edward <63171168+Stobers@users.noreply.github.com>
2022-07-12 20:39:54 +0100

    adding test loop

--- Src/gflame.cpp
+++ Src/gflame.cpp
@@ -7,15 +7,21 @@
 namespace Gflame{
     void init()
     {
-	amrex::Print() << "initialising domain \n";
+	amrex::Print() << "... initialising domain \n";
 	
-	amrex::ParmParse pp("dom");
-	pp.getarr("ncells",ncells);
-	pp.getarr("prob_lo",prob_lo);
-	pp.getarr("prob_hi",prob_hi);
-	pp.getarr("is_periodic",is_periodic);
-	pp.query("max_grid_size",max_grid_size);
+	amrex::ParmParse pp_dom("dom");
+	pp_dom.getarr("ncells",ncells);
+	pp_dom.getarr("prob_lo",prob_lo);
+	pp_dom.getarr("prob_hi",prob_hi);
+	pp_dom.getarr("is_periodic",is_periodic);
+	pp_dom.query("max_grid_size",max_grid_size);
 
+	amrex::ParmParse pp;
+	pp.query("nsteps",nsteps);
+	pp.query("end_time",end_time);
+	if (nsteps < 0 && end_time < 0)
+	    amrex::Abort("both nsteps and end_time negative");
+	    	
 	amrex::IntVect dom_lo(AMREX_D_DECL(0,0,0));
 	amrex::IntVect dom_hi(AMREX_D_DECL(ncells[0]-1,ncells[1]-1,ncells[2]-1));
 	amrex::Box domain(dom_lo, dom_hi);
@@ -23,9 +29,8 @@ namespace Gflame{
 	ba.maxSize(max_grid_size);
 	dm.define(ba);
 	
-	int ncomp = 2;
-	int ngrow = 0;
-	feild.define(ba, dm, ncomp, ngrow);
+	feild_new.define(ba, dm, feild_ncomp,feild_ngrow);
+	feild.define(ba, dm, feild_ncomp,feild_ngrow);
 	
 	amrex::RealBox real_box({AMREX_D_DECL(prob_lo[0],prob_lo[1],prob_lo[2])},{AMREX_D_DECL(prob_hi[0],prob_hi[1],prob_hi[2]}));
 	amrex::Array<int,AMREX_SPACEDIM> dom_periodic{AMREX_D_DECL(is_periodic[0],is_periodic[0],is_periodic[0])};
@@ -33,29 +38,44 @@ namespace Gflame{
 	dx = geom.CellSizeArray();
 
 	set_bc();
+	init_prob();
     }
 
-    
+
     void set_bc()
     {
-	static amrex::Vector<amrex::BCRec> bc(feild.nComp());
-	for(int n=0; n<feild.nComp(); ++n)
+	amrex::Vector<amrex::BCRec> bc(feild.nComp());
+	for (int n=0; n<feild.nComp(); ++n)
 	{
-	    for(int idim=0; idim<AMREX_SPACEDIM; ++idim)
+	    for (int idim=0; idim<AMREX_SPACEDIM; ++idim)
 	    {
-		if(geom.isPeriodic(idim))
+		if (geom.isPeriodic(idim))
 		{
-		    amrex::Print() << "is per \n";
 		    bc[n].setLo(idim, amrex::BCType::int_dir);
 		    bc[n].setHi(idim, amrex::BCType::int_dir);
 		}
 		else
 		{
-		    amrex::Print() << "is not per \n";
 		    bc[n].setLo(idim, amrex::BCType::foextrap);
 		    bc[n].setHi(idim, amrex::BCType::foextrap);
 		}
 	    }
+	}
+    }
+
+
+    void init_prob()
+     {
+	for (amrex::MFIter mfi(feild); mfi.isValid(); ++mfi)
+	{
+	    const amrex::Box& box = mfi.validbox();
+	    auto const& gnew = feild_new.array(mfi);
+	    auto const& g = feild.array(mfi);
+	    amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE(int i, int j, int k)
+		{
+		    g(i,j,k) = 0.5;
+		    gnew(i,j,k) = 1;
+		});
 	}
     }
 }
--- Src/gflame.hpp
+++ Src/gflame.hpp
@@ -9,17 +9,23 @@ namespace Gflame{
     // variables 
     //
     static int max_grid_size = 16;
+    static int nsteps = -1;
+    static amrex::Real end_time = -1;
     static amrex::Vector<int> ncells, is_periodic;
     static amrex::Vector<amrex::Real> prob_lo, prob_hi;
     static amrex::DistributionMapping dm;
     static amrex::BoxArray ba;
     static amrex::GpuArray<amrex::Real,AMREX_SPACEDIM> dx;
-    static amrex::MultiFab feild;
+    static amrex::MultiFab feild_new, feild;
     static amrex::Geometry geom;
+    static const unsigned int feild_ncomp = 2;
+    static const unsigned int feild_ngrow = 0;
+   
     //
     // functions
     //
     void init();
     void set_bc();
+    void init_prob();
 }
 #endif
